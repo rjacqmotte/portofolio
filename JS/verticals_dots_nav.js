8e8f64d832131bd9e7da29a54c4439eaa457eb51
renaud.jacqmotte <rjacqmotte@gmail.com>
2025-08-18 10:31:15 +0200

    clean and traduce in english comments in verticals_dots_nav.js

--- JS/verticals_dots_nav.js
+++ JS/verticals_dots_nav.js
@@ -6,13 +6,11 @@
 // VARIABLES
 // --------------------------------------------------
 
-// element container ul de la barre de navigation
+// Navigation bar container element
 const containerVerticalNav = document.body.querySelector("ul.vertical-nav__list-items");
 
 // Definition of variables that point to DOM objects using the .nav_page selector
 const arrayOfNavPage = document.getElementsByClassName("nav-page");
-console.log(`arrayOfNavPage is "${arrayOfNavPage}"`);
-console.log(arrayOfNavPage);
 
 // Definition of variables that point to the navigation dots, will be created and defined in main
 let arrayOfDotsNav;
@@ -30,7 +28,7 @@ let scrollCount = 0;
 // FUNCTIONS
 // --------------------------------------------------
 
-// Calcul du nombre de dots
+// Dot calculation
 // --------------------------------------------------
 
 // Calculates and returns the index of the element closest to the top of the page. Takes an array of elements.
@@ -42,7 +40,6 @@ const nearestFromTheTop = (arrayOfElements) => {
         let infoObject = element.getBoundingClientRect();
         arrayOfDistance.push(infoObject.top);
     })
-    console.log(arrayOfDistance);
 
     // Selects the visible element closest to the top of the screen
     // As the elements are naturally sorted in the order of the page,
@@ -50,42 +47,47 @@ const nearestFromTheTop = (arrayOfElements) => {
     return arrayOfDistance.findIndex(element => element > 0);
 }
 
-// Création des dots
+// Dot creation
 // --------------------------------------------------
 
-// reset et vide le container de la barre de navigation
+// Reset and empty the navigation bar container
 const resetVerticalNav = () => {
-    containerVerticalNav.innerHTML = "";
-};
+    if (containerVerticalNav) {
+        containerVerticalNav.innerHTML = "";
+    } else {
+        console.log("No container found for verticals dots nav. Need ul.vertical-nav__list-items")
+    };
+}
 
-// ajoute des points dans la barre de navigation
-//      1. crée li.vertical-nav__item à ajouter à ul.vertical-nav__list-item
-//      2. crée div.vertical-nav__point à ajouter à li.vertical-nav__item
+// Add dots to the navigation bar
+//      1. Create li.vertical-nav__item to add to ul.vertical-nav__list-items
+//      2. Create div.vertical-nav__point to add to li.vertical-nav__item
 const createDots = (numberOfDots) => {
-    const arrayOfDotsCont = [];
-    const arrayOfDots = [];
-    
-    for (let i = 0; i < numberOfDots; i++) {
-        const li = document.createElement("li");
-        li.classList.add("vertical-nav__item");
-        li.setAttribute("data-dot-index", i);
+    if (containerVerticalNav) {
+        for (let i = 0; i < numberOfDots; i++) {
+            const li = document.createElement("li");
+            li.classList.add("vertical-nav__item");
+            li.setAttribute("data-dot-index", i);
 
-        const dot = document.createElement("div");
-        dot.classList.add("vertical-nav__point");
-        
-        containerVerticalNav.appendChild(li);
-        li.appendChild(dot);
-    }    
+            const dot = document.createElement("div");
+            dot.classList.add("vertical-nav__point");
+
+            containerVerticalNav.appendChild(li);
+            li.appendChild(dot);
+        }
+    } else {
+        console.log("No container found for verticals dots nav. Need ul.vertical-nav__list-items")
+    }
 };
 
-// retourne l'array de dot
+// Returns the array of dots
 const getDotsNav = () => document.getElementsByClassName("vertical-nav__point");
 
 
-// Modification des dots
+// Dot modification
 // --------------------------------------------------
 
-// Reset la mise en page par défaut des dots
+// Reset dots to default style
 const resetDots = () => {
     Array.from(arrayOfDotsNav).forEach(dot => {
         dot.style.backgroundColor = "lightgrey";
@@ -94,20 +96,16 @@ const resetDots = () => {
     })
 }
 
-// Modify la mise en page du dot à mettre en avant
+// Highlight the active dot
 const makeBigger = (element) => {
     element.style.backgroundColor = "black";
     element.style.width = "12px";
     element.style.height = "12px";
 };
 
-// fonction callback pour l'écouter d'évenement scroll.
-// mets en avant le bon dots. 
-// Resets the dots layout, highlights the correct dot, manages the throttle.
+// Callback for scroll event listener.
+// Highlights the correct dot and manages throttle.
 const changeDot = () => {
-    // For debugging
-    console.log(`--- Fonction activée : ${scrollCount} --- `);
-
     resetDots();
     const navPageIndex = nearestFromTheTop(arrayOfNavPage);
     if (navPageIndex >= 0) {
@@ -125,37 +123,22 @@ const changeDot = () => {
 // MAIN
 // --------------------------------------------------
 
-// Test et debug
+// Test and debug
 // --------------------------------------------------
 
-console.log(nearestFromTheTop(arrayOfNavPage));
-console.log(arrayOfNavPage[nearestFromTheTop(arrayOfNavPage)]);
-
-console.log("-----------------------");
-console.log("arrayOfNavPage.length is");
-console.log(arrayOfNavPage.length);
-
-
-// Création dynamique des point de la barre verticale de navigation
+// Dynamic creation of vertical navigation dots
 // --------------------------------------------------
 
 resetVerticalNav();
 createDots(arrayOfNavPage.length);
 arrayOfDotsNav = getDotsNav();
 
-console.log(`arrayOfDotsNav is "${arrayOfDotsNav}"`);
-console.log(arrayOfDotsNav);
-
-
-
-// Event handling - scroll
+// Event handling
 // --------------------------------------------------
 
 window.addEventListener("scroll", (event) => {
 
     scrollCount++;
-    console.log(`Scroll détecter : ${scrollCount}`);
-
 
     // Set up a "throttle" to limit code activation by scroll events
     // Throttle test, exit the loop if active.
--- suivi_projet.md
+++ suivi_projet.md
@@ -4,12 +4,13 @@
 * corriger le module des barre de réseaux sociaux, lui faire des noms de classes cohérents, nettoyer ce qui n'est pas nécessaire, faire un git;
 * changer section.l-vertical-nav- par nav. ... ?
 
-## optionnel
 
+## optionnel
+* navigation dans la page à partir des points de navigation: cliquer sur un point amène au bon endroits de la page
 
 
 ## en cours
-* continuer la fonction create dots.
+
 
 
 ## fini
@@ -20,3 +21,6 @@
 * continuer et debugger le script pour la barre de nav
 * script pour activer les point de la barre de navigation vertical avec les titre h2 (finalement avec .nav-page)    
 * ajouter un throttle au script du nav dot bar
+* continuer la fonction create dots.
+* relir et mettre au propre verticals_dots_nav.js, effacer les console.log unitile.
+* bien tout traduire en anglais dans navdots.js
